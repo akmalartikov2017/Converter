72a05588a9bfe31879aab3801ae0173466e4daae
akmal <akmalartikov2017@gmail.com>
2021-12-10 16:59:19 +0300

    docx

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 "use strict"
+
 let a = document.querySelector("a");
 let input = document.querySelector("input");
 let convertApi = ConvertApi.auth({secret: 'qi4OYb8d6uYj9yAK'})
 let loader= document.querySelector(".lds-ripple")
 let label= document.querySelector(":before")
+
 input.addEventListener("change",convert);
 
 async function convert(e) {
-
-	try{
-		if(e.target.files[0]===undefined)return;
-
+	if(e.target.files[0]===undefined)return;
+	else if(/\.docx/.test(input.files[0].name)){
 		input.setAttribute("disabled","disabled");
 		a.style.display="none";
 		loader.style.display="inline-block";
@@ -18,14 +18,16 @@ async function convert(e) {
 		let params = convertApi.createParams();
 		params.add('file', e.currentTarget.files[0]);
 		let result = await convertApi.convert('docx', 'pdf', params);
-		
+
 		a.href=result.files[0].Url;
 		a.innerHTML=`Загрузить ${result.files[0].FileName}`;
 		loader.style.display="none"
 		a.style.display="block";
 		input.removeAttribute("disabled");
-	}catch(e){
-		console.log(e);
+	}
+
+	else{
+		alert("Только файлы в формате .docx")
 	}
 }
 
